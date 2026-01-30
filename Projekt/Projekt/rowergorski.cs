5c074ddd1357ef70ff0dc4bf69028d3e2290b22e
RafałSzwajcerCDV <rafal.szwajcer03@gmail.com>
2026-01-30 12:12:30 +0100

    Add files via upload

---
+++ Projekt/Projekt/rowergorski.cs
@@ -0,0 +1,10 @@
+﻿//dziedziczenie po sprzet
+class RowerGorski : Sprzet
+{
+    public RowerGorski()
+    {
+        TypRoweru = "Górski";
+    }
+
+    public override decimal CenaZaDzien => 30; //polimorfizm
+}
